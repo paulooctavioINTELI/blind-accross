57ecf48e66658ab441be9632671aceac71a5070d
Arthur Barbosa de Morais <bmartdomeu@gmail.com>
2024-10-14 20:34:58 -0300

    update index
    cirei os botoes que quando apertados diz se pode atravessar ou nao, em situações de teste

--- app/(stacks)/index.tsx
+++ app/(stacks)/index.tsx
@@ -1,71 +1,69 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Vibration } from 'react-native';
+import * as Speech from 'expo-speech';
 
+export default function Test() {
+  
+  const handleVibration = () => {
+    Vibration.vibrate(500);
+  }
+  
+  const speak = () => {
+    const phrase = 'Aguarde!'; 
+    Speech.speak(phrase, {
+      language: 'pt - BR', 
+      pitch: 1,       
+      rate: 1,
+    });
+  };
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+  const speakFree = () => {
+    const phrase = 'Pode atravessar!'; 
+    Speech.speak(phrase, {
+      language: 'pt - BR', 
+      pitch: 2,       
+      rate: 1.5,
+    });
+  };
 
-export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress = {() =>{
+        handleVibration();
+        speak();
+      }}>
+        <Text style={styles.buttonText}>Carro vindo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress = {() =>{
+        handleVibration();
+        speakFree();
+      }}>
+        <Text style={styles.buttonText}>Livre</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    display: 'flex',
+    flexDirection: 'row'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  button: {
+    backgroundColor: '#007bff',
+    height: 140,
+    width: 150,
+    borderRadius: 20,
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginBottom: 80
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    alignSelf: 'center'
   },
 });
